33cbcb7a1d531b7493eec9b046ee49f46a3fa8aa
ababup1192 <ababup1192@gmail.com>
2015-08-21 16:18:39 +0900

    First commit

---
+++ Multi.java
@@ -0,0 +1,11 @@
+public class Multi{
+
+    public static void main(String args){
+            MyThread thread = MyThread();
+
+            thread.run();
+
+            // とても重い処理を書いてみましょう。
+    }
+
+}
---
+++ MyThread.java
@@ -0,0 +1,8 @@
+public class MyThread extends Thread{
+
+    @Override
+    public void run(){
+        // とても重い処理を書いてみましょう。
+    }
+
+}
